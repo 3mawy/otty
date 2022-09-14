9105cbfd8640b49a194dd7b2eaffb86c0564b3c8
3mawy <m.3mawyy@gmail.com>
2022-09-14 02:30:26 +0200

    installed packages and edited home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Map from "../components/Map/Map";
+import ButtonsOverlay from "../components/ButtonsOverlay";
 
 const Home = () => {
-
     return (
         <div>
-            <Map/>
+
+            <div className={`relative z-30`}>
+                <Map/>
+            </div>
+
         </div>
     );
 };
-
 export default Home;
