93dfa946a2545fe09301b1d8463168c0b587635c
3mawy <m.3mawyy@gmail.com>
2022-09-16 02:09:17 +0200

    add placeholder icons and cats locations array

--- src/components/Map/LeafletMap.js
+++ src/components/Map/LeafletMap.js
@@ -1,7 +1,33 @@
 import React, { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import {ImCodepen} from "react-icons/im";
+import L from 'leaflet';
+import cat from "./img/position-gps-svgrepo-com.svg";
+import catred from "./img/position-gps-svgrepo-com-red.svg";
 
+const catIcon = new L.Icon({
+    iconUrl: cat,
+    iconRetinaUrl: cat,
+    iconAnchor: null,
+    popupAnchor: null,
+    shadowUrl: null,
+    shadowSize: null,
+    shadowAnchor: null,
+    iconSize: new L.Point(75, 75),
+});
+const locationIcon = new L.Icon({
+    iconUrl: catred,
+    iconRetinaUrl: catred,
+    iconAnchor: null,
+    popupAnchor: null,
+    shadowUrl: null,
+    shadowSize: null,
+    shadowAnchor: null,
+    iconSize: new L.Point(75, 75),
+});
+
+export { catIcon, locationIcon };
 
 const Map = () => {
     // visitor geoLocalisation on the Map
@@ -18,10 +44,18 @@ const Map = () => {
         }, []);
 
         return position === null ? null : (
-            <Marker position={position}  >
+            <Marker position={position} icon={locationIcon}>
                 <Popup>You are here</Popup>
             </Marker>
         );
+    }
+    function CatsLocations() {
+        const [catsLocations, setCatsLocations] = useState([{position:[31.0471,  31.8236]},{position:[29.7471,  31.8236]}]);
+
+        return catsLocations.map((location) =>
+            (<Marker position={location.position}  icon={catIcon} >
+                <Popup>Meow!</Popup>
+            </Marker>))
     }
 
     return (
@@ -37,6 +71,8 @@ const Map = () => {
             />
 
             <LocationMarker />
+            <CatsLocations />
+            <img src={catIcon}/>
         </MapContainer>
     );
 };
