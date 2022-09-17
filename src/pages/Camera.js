8e0014f0b41ef9b0cfedcd8d31244fae32190629
3mawy <m.3mawyy@gmail.com>
2022-09-17 23:45:29 +0200

    add Camera seprate route

---
+++ src/pages/Camera.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+import ReactCamera from "../components/Camera/ReactCamera";
+import {BiCamera, BiUpload} from "react-icons/bi";
+
+const Camera = () => {
+    //ReactCamera
+
+    return (
+        <div>
+            <ReactCamera />
+        </div>
+    );
+};
+
+export default Camera;
