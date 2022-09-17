ea4de4fe43e95204fdfb4adc0640c33c9575fab5
3mawy <m.3mawyy@gmail.com>
2022-09-18 01:57:44 +0200

    edit posting form

---
+++ src/pages/AddPostForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Map from "../components/Map/Map";
+import ReactImagePreview from "../components/Camera/ReactImagePreview";
+
+const AddPostForm = ({children}) => {
+    return (
+        <div>
+            <Map className={`z-20`}/>
+            <div className={`bg-red-600 h-screen w-screen z-50`}>
+                {children}
+            </div>
+        </div>
+    );
+};
+
+export default AddPostForm;
